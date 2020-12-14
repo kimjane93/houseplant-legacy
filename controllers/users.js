30203486c538ea1ad7362aeeb2915e3273902907
kimjane93 <kjchadwell93@gmail.com>
2020-12-13 23:01:44 -0500

    set up my profile path and basic webpage

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,16 @@
 const User = require('../models/user')
 
 module.exports = {
-    
+    myProfile,
+}
+
+function myProfile(req, res){
+    User.findById(req.user._id)
+    .populate("plantCollection")
+    .then((user)=>{
+        res.render('users/profile', {title: 'My Profile', user: user})
+    })
+    .catch((err)=>{
+        res.redirect('/')
+    })
 }
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,12 @@
 const router = require('express').Router()
+const usersCtrl = require('../controllers/users')
+
+router.get('/profile', isLoggedIn, usersCtrl.myProfile)
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/auth/google");
+  }
 
 module.exports = router
