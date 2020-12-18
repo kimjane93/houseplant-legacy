cf9ceaf1689837a5edbae43177f794ef6261dafe
kimjane93 <kjchadwell93@gmail.com>
2020-12-18 13:45:29 -0500

    add reply path to message show page

--- controllers/messages.js
+++ controllers/messages.js
@@ -2,6 +2,9 @@ const Message = require('../models/message')
 
 module.exports = {
     index, 
+    create,
+    show,
+    respond,
 }
 
 function index(req, res){
@@ -11,5 +14,40 @@ function index(req, res){
     })
     .catch((err)=>{
         console.log(err)
+    })
+}
+
+function create(req, res){
+    req.body.postedBy = req.user.name
+    req.body.avatar = req.user.avatar
+    Message.create(req.body)
+    .then(()=>{
+        res.redirect('/messages')
+    })
+}
+
+function show(req, res){
+    Message.findById(req.params.id)
+    .then((message)=>{
+        res.render('messages/show', {title: req.user, user: req.user, message: message})
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+
+function respond(req, res){
+    Message.findById(req.params.id)
+    .then((message)=>{
+        req.body.postedBy = req.user.name
+        req.body.avatar = req.user.avatar
+        message.replies.push(req.body)
+        message.save()
+        .then(()=>{
+            res.redirect(`/messages/${message._id}`)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
     })
 }
