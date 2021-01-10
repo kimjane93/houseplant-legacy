ae52c4a34468c06f83f226a32a2c59cd411997d6
kimjane93 <kjchadwell93@gmail.com>
2021-01-09 23:25:27 -0500

    add properties to error handler to allow custom error page

--- server.js
+++ server.js
@@ -65,6 +65,8 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
+  user = req.user
+  title = 'What Did You Do?'
 
   // render the error page
   res.status(err.status || 500);
